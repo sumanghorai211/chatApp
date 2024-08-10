b41ed72fdb77533f71b7c8e7015085fb1ba7c487
suman-ghorai <sumanghorai211@gmail.com>
2024-08-11 00:37:38 +0530

    5th commit

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const { app, server } = require("./socket/index");
 // const app = express()
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: "https://endearing-gumdrop-ad1ace.netlify.app",
     credentials: true,
   })
 );
